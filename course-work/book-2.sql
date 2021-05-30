e5cd5e0efd741e55ec379c0c6d95d191ae08aab6
Todd Spainhour <toddspainhour@gmail.com>
2021-05-30 13:25:09 -0500

    added join practice questions

--- course-work/book-2.sql
+++ course-work/book-2.sql
@@ -94,16 +94,8 @@ where s.deposit > 5000 OR s.payment_method = 'americanexpress'
 
 select *
 from employees e 
-where e.first_name like 'M%' 
-OR e.first_name like '_E%' 
-
-
-
-
-
-
-
-
+where e.first_name like 'M%'
+or e.first_name like '%E' 
 
 
 
@@ -166,4 +158,67 @@ select *
 from vehicleTypes
 	
 	
-	
+
+
+
+----- Book 2 Chapter 3 JOINING DATA -----
+
+--Practice Question - Get a list of the sales that were made for each sales type.
+
+select *
+from sales s 
+inner join salestypes st
+on s.sales_type_id = st.sales_type_id 
+
+
+
+
+--Practice Question - Get a list of sales with the VIN of the vehicle, 
+--the first name and last name of the customer, 
+--first name and last name of the employee who made the sale 
+--and the name, city and state of the dealership.
+
+select 
+	vin, 
+	c.first_name as "customer first name", 
+	c.last_name as "customer last name", 
+	e.first_name as "employee first name", 
+	e.last_name  as "employee last name",
+	d.business_name,
+	d.city as "dealership city",
+	d.state as "dealership state"
+from sales s 
+join vehicles v 
+on s.vehicle_id = v.vehicle_id 
+join customers c 
+on s.customer_id = c.customer_id 
+join employees e 
+on s.employee_id = e.employee_id 
+join dealerships d 
+on s.dealership_id = d.dealership_id 
+
+
+
+
+--Practice Question - Get a list of all the dealerships and the employees, if any, working at each one.
+
+select 
+	d.business_name as "dealership name",
+	e.first_name as "employee first name",
+	e.last_name as "employee last name"
+from dealerships d 
+join dealershipemployees de 
+on d.dealership_id = de.dealership_id 
+join employees e 
+on de.dealership_employee_id = e.employee_id 
+order by business_name 
+
+
+
+--Practice Question - Get a list of vehicles with the names of the body type, make, model and color.
+ 
+
+
+
+
+
